4e4771b7ce6d06af8fd1f9d2e2cee2324cc96584
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-10-21 08:33:36 +0530

    basics:number,boolean

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+//////Numbers
+
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+// fn main(){
+//     let x:u32=1;
+//     println!("{}",x);
+
+// }
+
+// fn main(){
+//     let x:i32=-1;
+//     println!("{}",x);
+// }
+
+//Booleans
+//simple boolean function
+fn main(){
+    let a=5;
+   if  is_even(a){
+    println!("{} is even",a);
+
+   } else{
+    println!("{} is odd",a  );
 }
+}
+
+fn is_even(a:u32)->bool{
+    return a%2==0;
+}
+///
