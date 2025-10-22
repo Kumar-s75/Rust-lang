369ea3c72dd0be8aa3c33fee420abd461e258234
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-10-22 16:38:47 +0530

    structs again

--- src/main.rs
+++ src/main.rs
@@ -216,3 +216,99 @@ fn main(){
 
 }
 
+///////Structs in Rust
+struct User{
+   active:bool,
+   username:String,
+   email:String,
+   sign_in_count:u64,
+}
+
+fn main() {
+    let user1=User{
+        active:true,
+        username:String::from("someusername123"),
+        email:String::from("someone@example.com"),
+        sign_in_count:1,
+    };
+    print!("User 1 username:{:?}",user1.username);
+}
+
+////only stack types in struct
+// code 1)
+struct User{
+    active:bool,
+    sign_in_count:u64,
+}
+fn main(){
+    let mut user1=User{
+        active:true,
+        sign_in_count:1,
+    };
+    print_name(user1);
+    print!("User 1 username:{}",user1.active());
+}
+/*error - cannot use borrowed value */
+fn print_name(user1:User){
+   print!("User 1 username:{}",user1.active());
+}
+
+
+/*copy trait */
+
+#[derive(Copy,Clone)]
+struct User{
+    active:bool,
+    sign_in_count:u64,
+}
+
+fn main(){
+    let mut user1=User{
+        active:true,
+        sign_in_count:1,
+    };
+    print_name(user1);
+    print!("User 1 username: {}",user1.active);
+}
+fn print_name(user1:User){
+    print!("User 1 username:{}",user1.active);
+}
+/*error goes away as user1 is copied */
+/*Strings */
+struct User{
+    active:bool,
+    sign_in_count:u64,
+    username:String,
+}
+fn main(){
+  let mut user1:User{
+    active:true,
+    sign_in_count:1,
+    username:"harkirat".to_string()
+  };
+
+  change_name(user1);
+  print!("User 1 username:{}",user1.active);
+}
+fn change_name(user1:User){
+   print!("User 1 username:{:?}",user1.active);
+}
+
+/*in case of strings we cannot use copy trait, use clone trait instead.*/
+struct User{
+  active:bool,
+  sign_in_count:u64,
+  username:String,
+}
+fn main(){
+    let mut user1:User{
+        active:true,
+        sign_in_count:1,
+        username:"harkirat".to_string()
+    };
+    change_name(user1.clone())
+    print!("User 1 username:{}",user1.active);
+}
+fn change_name(user:User1){
+    print!("User 1 username :{:?}",user1.active);
+}
