cc5208178814a31de168b281a5412064d20d11f3
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-10-23 00:24:14 +0530

    enums

--- src/main.rs
+++ src/main.rs
@@ -345,3 +345,37 @@ fn main(){
  fn main(){
     Rect::print_str();
  }
+
+ /*enums*/
+
+ enum Direction{
+    North,
+    East,
+    South,
+    West
+ }
+ fn main(){
+    let my_direction=Direction::North;
+    let new_direction=my_direction;
+    move_around(new_direction);
+ }
+ fn move_around(direction:Direction){
+    //implements logic to move a character around
+ }
+/* enums with values*/
+enum Shape(){
+    Circle(f64),
+    Square(f64),
+    Rectangle(f64,f64)
+
+}
+
+fn calculated_area(shape:Shape)->f64{
+    return 0
+}
+
+fn main(){
+    let circle=Shape::Circle(5.0);
+    let square=Shape::Square(4.0);
+    let rectangle=Shape::Rectangle(3.0,6.0)
+}
