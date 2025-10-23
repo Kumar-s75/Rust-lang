fa2fed837cef4d7b6eb9ba4904fc110904453897
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-10-23 21:42:26 +0530

    option enum

--- src/main.rs
+++ src/main.rs
@@ -416,3 +416,23 @@ fn main(){
     let greeting_file_result=fs::read_to_string("hello to txt");
     print!("{}",greeting_file_result.unwrap());
  }
+
+/*option enums*/
+/*instead of null return option */
+fn main(){
+    let my_string=String::from("raman");
+    match find_first_a(my_string){
+        Some(index)=>println!("The letter 'a' is found at index:{}",index),
+        None=>println!("The letter 'a' is not found in String.");
+    }
+
+}
+fn find_first_a(s:String)->Option<i32>{
+    for(index,character) in s.chars().enumerate(){
+        if character=='a'{
+            return Some(index as i32);
+
+    }
+}
+return None;
+}
